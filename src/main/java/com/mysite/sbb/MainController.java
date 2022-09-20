4c72a5c88195d874e49484a6376d973d6181f6c8
tooffuu <thdud8866@naver.com>
2022-09-20 17:12:23 +0900

    구구단

--- src/main/java/com/mysite/sbb/MainController.java
+++ src/main/java/com/mysite/sbb/MainController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class MainController {
+    int increaseNum = 0;
 
     @RequestMapping("/sbb")
     @ResponseBody
@@ -23,7 +24,7 @@ public class MainController {
                 </form>
                 """;
     }
-    
+
     @PostMapping("/page2")
     @ResponseBody
     public String showPage2Post(@RequestParam(value = "age", defaultValue = "0") int age) {
@@ -32,5 +33,34 @@ public class MainController {
                 <h1>입력된 나이 : %d</h1>
                 <h1>POST 방식으로 옴</h1>
                 """.formatted(age);
+    }
+
+    @GetMapping("/plus")
+    @ResponseBody
+    public int plus(int a, int b) {
+        return a + b;
+    }
+
+    @GetMapping("/minor")
+    @ResponseBody
+    public int minus(int a, int b) {
+        return a - b;
+    }
+
+    @GetMapping("/increase")
+    @ResponseBody
+    public int increases() {
+        return increaseNum++;
+    }
+
+
+    @GetMapping("/gugudan")
+    @ResponseBody
+    public String gugudan(int dan, int limit) {
+        String result = "";
+        for (int i = 1; i <= limit ; i++) {
+            result += dan + " * " + i + " = " + (dan * i) + "<br />\n";
+        }
+        return result;
     }
 }
