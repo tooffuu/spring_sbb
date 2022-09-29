8e313db3094c2b8461f7af77e7b06b6ba5afe110
tooffuu <thdud8866@naver.com>
2022-09-29 15:21:28 +0900

    answer controller 생성

---
+++ src/main/java/com/mysite/sbb/answer/controller/AnswerController.java
@@ -0,0 +1,25 @@
+package com.mysite.sbb.answer.controller;
+
+import com.mysite.sbb.question.domain.domain.Question;
+import com.mysite.sbb.question.domain.service.QuestionService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/answer")
+@RequiredArgsConstructor
+public class AnswerController {
+    private final QuestionService questionService;
+
+    @PostMapping("/create/{id}")
+    public String createAnswer(Model model, @PathVariable("id") Integer id, @RequestParam String content) {
+        Question question = questionService.getQuestion(id);
+
+        return String.format("redirect:/question/detail/%s", id);   // 답변 저장, 답변이 달린 페이지로 return
+    }
+}
